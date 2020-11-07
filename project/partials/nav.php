665411ab676723ec37c7793a401c7d15809a23cb
Merin J Vattathara <mjv43@afsconnect1.njit.edu>
2020-11-06 22:51:54 -0500

    nav

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -4,16 +4,19 @@
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
 <nav>
-<ul class="nav">
-    
-    <?php if (!is_logged_in()): ?>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
-    <?php endif; ?>
-    <?php if (is_logged_in()): ?>
+    <ul class="nav">
         <li><a href="home.php">Home</a></li>
-        <li><a href="profile.php">Profile</a></li>
-        <li><a href="logout.php">Logout</a></li>
-    <?php endif; ?>
-</ul>
+        <?php if (!is_logged_in()): ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+        <?php endif; ?>
+        <?php if (has_role("Admin")): ?>
+            <li><a href="test_create_egg.php">Create Egg</a></li>
+            <li><a href="test_list_egg.php">View Eggs</a></li>
+        <?php endif; ?>
+        <?php if (is_logged_in()): ?>
+            <li><a href="profile.php">Profile</a></li>
+            <li><a href="logout.php">Logout</a></li>
+        <?php endif; ?>
+    </ul>
 </nav>
