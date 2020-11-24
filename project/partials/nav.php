c589347278235ec0558ab1ce13baaf662be40023
Merin J Vattathara <mjv43@afsconnect1.njit.edu>
2020-11-24 03:52:57 -0500

    nav

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -11,13 +11,12 @@ require_once(__DIR__ . "/../lib/helpers.php");
             <li><a href="register.php">Register</a></li>
         <?php endif; ?>
         <?php if (has_role("Admin")): ?>
-            <li><a href="test_create_products.php">Create Products</a></li>
-            <li><a href="test_list_products.php">View Products</a></li>
-            <li><a href="test_create_cart.php">Create Cart</a></li>
-            <li><a href="test_edit_cart.php">Edit Cart</a></li>
-            <li><a href="test_list_cart.php">View Cart</a></li>
+            <li><a href="productCreation.php">Create Products</a></li>
         <?php endif; ?>
+        <li><a href="productSearch.php">View Products</a></li>
         <?php if (is_logged_in()): ?>
+            <li><a href="cartCreation.php">Add to Cart</a></li>
+            <li><a href="cartView.php">View Cart</a></li>
             <li><a href="profile.php">Profile</a></li>
             <li><a href="logout.php">Logout</a></li>
         <?php endif; ?>
