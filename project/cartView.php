df3b38ce57586d3f3b0256ad88b6a1328aa8654b
Merin J Vattathara <mjv43@afsconnect1.njit.edu>
2020-11-24 04:27:26 -0500

    cart view

--- project/cartView.php
+++ project/cartView.php
@@ -1,7 +1,7 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
 if (!is_logged_in()) {
-    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    //this will redirect to login and kill the rest of this script
     flash("You don't have permission to access this page");
     die(header("Location: login.php"));
 }
