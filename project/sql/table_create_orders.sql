1b0552af3f590fe4c64cf942fa0733d8100c9aa4
Merin J Vattathara <mjv43@afsconnect1.njit.edu>
2020-11-25 02:10:02 -0500

    order tables

---
+++ project/sql/table_create_orders.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Orders
+(
+    id         int auto_increment,
+    user_id    int,
+    address    varchar(60) NOT NULL,
+    payment_method text,
+    total_price      decimal(12, 2) default 0.00,
+    created     TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    primary key (id),
+    foreign key (user_id) references Users (id),
+    UNIQUE KEY (user_id)
+)
+
